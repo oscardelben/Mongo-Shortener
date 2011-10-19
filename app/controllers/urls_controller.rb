4f1b57de8b113e137a92e148555f582c32a1fde4
MikeW <mike@propertybase.com>
2011-10-19 19:11:09 +0200

    - shortening is done Base62 (fewer characters) - identical urls get same shortened version - non existing short urls get n/a page

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -5,8 +5,11 @@ class UrlsController < ApplicationController
   end
   
   def make_short
-    @shortened_url = ShortenedUrl.new(:url => params[:url])
-    @shortened_url.save!
+    @shortened_url = ShortenedUrl.first(:conditions => { :url => params[:url] })
+    if @shortened_url.nil?
+      @shortened_url = ShortenedUrl.new(:url => params[:url])
+      @shortened_url.save!
+    end    
     
     shortened = url_for(:action => :show, :id => @shortened_url.shortened)
 
@@ -18,15 +21,18 @@ class UrlsController < ApplicationController
   
   def show
     @shortened_url = ShortenedUrl.first(:conditions => { :shortened => params[:id] })
-
-    respond_to do |format|
-      format.html do
-        @shortened_url.visited!
-        redirect_to(@shortened_url.url)
+    if @shortened_url.nil?
+      render "na"
+    else
+      respond_to do |format|
+        format.html do
+          redirect_to(@shortened_url.url)
+        end
+        format.stats { render :layout => "application.html" }
+        format.preview { render :layout => "application.html" }
       end
-      format.stats { render :layout => "application.html" }
-      format.preview { render :layout => "application.html" }
     end
+
   end
   
 end
